0ebf3539039423be6236b9fe893a962c5aaf9dc7
saurabhkul11 <saurabhkul18@gmail.com>
2023-06-16 11:46:17 +0530

    first commit

--- app/src/main/java/com/example/managerapp/repo/MyRepository.kt
+++ app/src/main/java/com/example/managerapp/repo/MyRepository.kt
@@ -3,7 +3,6 @@ package com.example.managerapp.repo
 import com.example.managerapp.api.ApiService
 
 class MyRepository(private var apiService: ApiService) {
-
   suspend  fun getData()=apiService.getData()
 
 }
